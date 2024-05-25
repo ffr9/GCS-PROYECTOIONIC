68a1fefdacf08399665a42f1dc1bf53210aa5a52
Juan Carlos Babero Castejon <jcbc3@alu.ua.es>
2024-05-25 17:35:58 +0200

    pagina acabada

--- foodlink/src/app/creacionevento/creacionevento.page.ts
+++ foodlink/src/app/creacionevento/creacionevento.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-creacionevento',
@@ -6,16 +7,43 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./creacionevento.page.scss'],
 })
 export class CreacioneventoPage implements OnInit {
+  startDate: string = ''; // Inicializando la propiedad
+  endDate: string = ''; // Inicializando la propiedad
 
-  constructor() { }
+  constructor(private alertController: AlertController) { }
 
-  ngOnInit() {
+  ngOnInit() { }
+
+  async openCalendar(type: string) {
+    const alert = await this.alertController.create({
+      header: type === 'start' ? 'Selecciona fecha de inicio' : 'Selecciona fecha de fin',
+      inputs: [
+        {
+          name: 'date',
+          type: 'date',
+          value: new Date().toISOString().split('T')[0]
+        }
+      ],
+      buttons: [
+        {
+          text: 'Cancelar',
+          role: 'cancel',
+          handler: () => {
+            console.log('Confirm Cancel');
+          }
+        }, {
+          text: 'Ok',
+          handler: (data) => {
+            if (type === 'start') {
+              this.startDate = data.date;
+            } else {
+              this.endDate = data.date;
+            }
+          }
+        }
+      ]
+    });
+
+    await alert.present();
   }
-
-  // Función para abrir el calendario
-  openCalendar(type: string) {
-    console.log('Abriendo calendario para:', type);
-    // Aquí puedes agregar la lógica para abrir el calendario según el tipo de fecha (inicio o fin)
-  }
-
 }
